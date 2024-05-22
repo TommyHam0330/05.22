d6653bac09d9d91fcca20a2778b2daa5e16ce7eb
TommyHam0330 <165112022+TommyHam0330@users.noreply.github.com>
2024-05-22 12:31:11 +0900

    d

---
+++ ddd.py
@@ -0,0 +1,6 @@
+from house import HouseRabbit
+from rabbit import Rabbit
+from 22class3 import MountainRabbit
+
+a = HouseRabbit()
+a.eat_feed()
--- house.py
+++ house.py
@@ -1,8 +1,10 @@
 class HouseRabbit (Rabbit):
     shape = "house"
     
-    def __init__(self, x, y):
+    def __init__(self, x=None, y=None):
+
         self.goto(x, y)
         
     def eat_feed (self):
         print("토끼가 사료를 먹습니다.")
+
